eb9193ecb5b70c778771f6eb3c780539c2cde8b5
Cafer <cafererhan@gmail.com>
2023-08-09 16:12:01 +0300

    Reformat

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 import './assets/style.css'
 
-const app = createApp(App)
-    .use(router)
+const app = createApp(App).use(router)
 
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,20 +5,16 @@
 </style>
 <template>
   <body>
-  <div class="start">
+    <div class="start">
+      <h2 class="text-5xl font-extrabold md:text-5xl mt-5 mb-5">About Me</h2>
 
-    <h2 class="text-5xl font-extrabold  md:text-5xl mt-5 mb-5">About Me</h2>
-
-    <h2 class="text-3xl font-light md:text-3xl mb-3">I graduated from Süleyman Demirel University in 2021 as a Computer
-      Engineer. During my university education, I
-      gained 2 years of mobile programming (Flutter) experience and worked with customers. I developed web services
-      using NodeJS, Java.
-
-      I am a person who loves his job and pays attention to details. I attach great importance to learning new
-      technologies and improving my skills.</h2>
-
-
-  </div>
-
+      <h2 class="text-3xl font-light md:text-3xl mb-3">
+        I graduated from Süleyman Demirel University in 2021 as a Computer Engineer. During my
+        university education, I gained 2 years of mobile programming (Flutter) experience and worked
+        with customers. I developed web services using NodeJS, Java. I am a person who loves his job
+        and pays attention to details. I attach great importance to learning new technologies and
+        improving my skills.
+      </h2>
+    </div>
   </body>
 </template>
