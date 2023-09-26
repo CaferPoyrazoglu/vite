ae461f573c51fddd9ba08af5d8d5d720b322008d
Cafer <cafererhan@gmail.com>
2023-09-26 11:45:53 +0300

    Squashed commit of the following:
    commit 2a0d1563eaacc9a5e5352b377f6a0f86c3f5ce8f
    Author: Cafer <cafererhan@gmail.com>
    Date:   Tue Sep 26 11:45:20 2023 +0300
    
        Test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+//TEST
 const router = createRouter({
   history: createWebHistory(),
   routes: [
